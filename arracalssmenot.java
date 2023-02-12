548889547291919581e772723fb36e13f1166223
Asadullah <88024587+Asadullah-nadeem@users.noreply.github.com>
2023-02-12 11:18:06 +0530

    update

--- arracalssmenot.java
+++ arracalssmenot.java
@@ -1,18 +1,35 @@
 package lecture;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class arracalssmenot {
     public static void main(String[] args) {
-        int a[] = new int[5];
+        int a[][] = new int [2][2];
         System.out.print("Enter Array Elements: ");
         Scanner s = new Scanner(System.in);
-        for (int i = 0; i<5; i++)
+        for (int i = 0; i<2; i++)
         {
-            a[i] = s.nextInt();
+            for (int j =0; j<2; j++){
+                a[i] [j]= s.nextInt();
+            }
 
-        }for (int b : a){
-            System.out.println(b + " ");
+
         }
+//        Arrays.sort(a);
+        System.out.print("\nMatrix Elements:\n");
+        for (int i = 0; i<2; i++)
+        {
+            for (int j =0; j<2; j++){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+
+
+        }
+        //        for (int b : a){
+//            System.out.println(b + " ");
+//        }
     }
 }
