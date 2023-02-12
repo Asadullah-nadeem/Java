0b2c9061cb4533b77e9ec3c25b3af60e7ae9c38e
Asadullah <88024587+Asadullah-nadeem@users.noreply.github.com>
2023-02-13 00:49:28 +0530

    update

--- Instead/Main.java
+++ Instead/Main.java
@@ -2,7 +2,7 @@ package Instead;
 
 public class Main {
     public static void main(String[] args) {
-        Person myObj = new Person();
+        Instead.Person myObj = new Instead.Person();
         myObj.setName("John");
         System.out.println(myObj.getName());
     }
